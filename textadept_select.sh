07e0764520b80c7747e0f52a6185220126625195
Al Zatv <al.zatv@gmail.com>
2020-02-23 22:11:46 +0300

    +

--- textadept_select.sh
+++ textadept_select.sh
@@ -11,8 +11,10 @@ if [ "$1" = "--install" ]; then
   wget https://foicica.com/textadept/download/textadept_LATEST.x86_64.tgz
   tar zxvf textadept_LATEST.x86_64.tgz
   rm textadept_LATEST.x86_64.tgz
-  [ -f $PWD/textadept*/textadept-curses ] || { echo "check tools directory. need only one textadept*/textadept-turses"; exit 1; }
-  ln -s $PWD/textadept* textadept
+  extr_ta_dir=$PWD/textadept_*.x86_64
+  [ -f $extr_ta_dir/textadept-curses ] || { echo "check tools directory. need only one textadept*/textadept-turses"; exit 1; }
+  rm -f textadept
+  ln -s $extr_ta_dir textadept
 else
   echo "--install not specified,so not installing"
 fi
@@ -20,6 +22,7 @@ fi
 echo "selecting"
 TADIR=~/tools/textadept
 [ -f $TADIR/textadept-curses ] || { echo "Not installed in proper way! check $TADIR!"; exit 1;}
+sudo rm -f /usr/bin/tag /usr/bin/ta
 sudo ln -s $TADIR/textadept /usr/bin/tag
 sudo ln -s $TADIR/textadept-curses /usr/bin/ta
 echo "check ta and tag:"
